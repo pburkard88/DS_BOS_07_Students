12d3a12a78c136e3af41d7c66ffedbace7e22bd2
Andrew Reichel <andrew.reichel@gmail.com>
2015-11-03 21:17:56 -0500

    removing debug scripts

--- AndrewReichel/Homework/HW_1/Test_Scripts.sql
+++
@@ -1,5 +0,0 @@
-SELECT Batting.playerID, awardID, Count(*) AS Count 
-FROM AwardsPlayers
-JOIN Batting
-ON AwardsPlayers.playerID = Batting.playerID
-WHERE awardID = "Triple Crown"#or awardID = "MVP"
